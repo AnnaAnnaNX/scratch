b28bc7149b24d5a282f0193abaa589dcb62a6e76
AnnaAnnaNX <anna.annanx@rambler.ru>
2021-01-02 14:12:34 +0300

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,35 @@
       sm="12"
       md="3"
     >
-      <v-btn type='button' value="add circle" @click="add">Add scratch</v-btn>
+      <div>
+        <v-btn type='button' @click="add">Add scratch</v-btn>
+      </div>
+      <div>
+        <v-select
+            :items="items"
+            v-model="selectedScratch"
+          ></v-select>
+          {{ selectedScratch }}
+      </div>
+      <div>
+        <v-text-field
+          label="x"
+          v-model="coord.x"
+        ></v-text-field>
+        <v-text-field
+          label="y"
+          v-model="coord.y"
+        ></v-text-field>
+        <v-text-field
+          label="z"
+          v-model="coord.z"
+        ></v-text-field>
+        <v-btn
+         type='button'
+          @click="relocate"
+          :disabled="selectedScratch === null"
+        >Relocate scratch</v-btn>
+      </div>
     </v-col>
   </v-row>
 </template>
@@ -31,12 +59,39 @@ export default {
       illo: null,
       scratches: [],
       currentScratch: null,
+      selectedScratch: null,
+      coord: {
+        x: null,
+        y: null,
+        z: null,
+      },
     }
+  },
+  computed: {
+    items() {
+      return Object.keys(this.scratches);
+    },
+  },
+  watch: {
+    selectedScratch(newValue) {
+      console.log('changed selectedScratch');
+      console.log(newValue);
+      this.currentScratch = this.scratches[newValue];
+      if (!this.currentScratch) {
+        this.coord.x = null;
+        this.coord.y = null;
+        this.coord.z = null;
+        return;
+      }
+      this.coord.x = this.currentScratch.translate.x;
+      this.coord.y = this.currentScratch.translate.y;
+      this.coord.z = this.currentScratch.translate.z;
+    },
   },
   methods: {
     add() {
       console.log('add circle');
-      const newCurrent = this.currentScratch.copy({
+      const newCurrent = this.currentScratch.copyGraph({
         // overwrite original options
         translate: { x: this.currentScratch.translate.x + 30 },
         color: '#C25',
@@ -44,6 +99,15 @@ export default {
       this.currentScratch = newCurrent;
       this.scratches.push(this.currentScratch);
       // update & render
+      this.illo.updateRenderGraph();
+    },
+    relocate() {
+      console.log('relocate');
+
+      this.currentScratch.translate.x = this.coord.x;
+      this.currentScratch.translate.y = this.coord.y;
+      this.currentScratch.translate.z = this.coord.z;
+
       this.illo.updateRenderGraph();
     },
   },
@@ -55,13 +119,24 @@ export default {
       element: '.zdog-canvas',
     });
 
-    // add circle
-    this.currentScratch = new Zdog.Ellipse({
+    const group = new Zdog.Group({
       addTo: this.illo,
-      diameter: 80,
-      stroke: 20,
-      color: '#636',
+      translate: { z: 20 },
     });
+    // eye white first
+    new Zdog.Ellipse({
+      addTo: group,
+      width: 160,
+      height: 80,
+    });
+    // then iris
+    new Zdog.Ellipse({
+      addTo: group,
+      diameter: 70,
+    });
+
+    // add circle
+    this.currentScratch = group;
 
     this.scratches.push(this.currentScratch);
 
